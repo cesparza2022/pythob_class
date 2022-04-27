e9e4fa4e6e3bbc725ed4d7e3edb5256702e7bc5d
Cesar Esparza <cesardash@gmail.com>
2022-04-27 18:56:20 -0500

    Ordenar el repositorio

---
+++ docs/template.py
@@ -0,0 +1,29 @@
+## name:  [Name]    Version [#.#]
+
+## description
+
+## synopsis
+ program_name [options list] arguments
+
+## examples
+
+## arguments
+ [name] description
+ [name] description
+
+## software requirements
+[This includes runtime environments and shared libraries required to run the program]
+
+## input 
+[files or directories used to run the program and formats]
+
+## output
+[file names and formats]
+
+## authors
+## dateCreated: [YYYY-MM-DD] -  author: [name]
+## dateModified [YYYY-MM-DD] - contributor: [describe the modification]
+
+## URL
+
+## Steps
