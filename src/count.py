1e048e70097a77911d470705bfd96594ef0d49e5
Cesar Esparza <cesardash@gmail.com>
2022-04-27 18:37:46 -0500

    Ordenar el repositorio

---
+++ src/count.py
@@ -0,0 +1,3 @@
+arreglo = input("ingresa el arreglo: ")
+
+print(f'Freceuncia de nucleotido\n" {str(arreglo)}\n A: {arreglo.count("A")} C: {arreglo.count("C")} T: {arreglo.count("T")} G: {arreglo.count("G")}')
