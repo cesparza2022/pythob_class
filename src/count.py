fb1553c90b694ea509b18d3361ec6dd6373454bf
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-05-04 00:10:08 -0500

    Repetición del archivo

--- src/count.py
+++
@@ -1,3 +0,0 @@
-arreglo = input("ingresa el arreglo: ")
-
-print(f'Freceuncia de nucleotido\n" {str(arreglo)}\n A: {arreglo.count("A")} C: {arreglo.count("C")} T: {arreglo.count("T")} G: {arreglo.count("G")}')
