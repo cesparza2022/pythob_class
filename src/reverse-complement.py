52194b530f10c78f34622b8bd5b8c223dd6488c9
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-05-04 12:30:19 -0500

    Script para sacar la cadena reversa complemetaria

--- src/reverse-complement.py
+++ src/reverse-complement.py
@@ -1,26 +1,12 @@
-'''
-NAME
-	reverse-complement.py
-    
-VERSION
-    1.0
-    
-AUTHOR
-	
-    
-DESCRIPTION
-	Make the reverse complement of DNA sequence
-    
-CATEGORY
-	Genomic Sequence
-    
-USAGE
+nucleotidos = ["A", "T","C","G"]
+reversos = {"A":"T", "T":"A","C": "G","G":"C"}
 
-    % python reverse-complement.py -i 
-    
-    example
-    
-    % python reverse-complement -i
+def reverso(arreglo):
+  return"".join( reversos[nuc] for  nuc in arreglo)
 
-'''
+arreglo = input("ingresa la secuencia: ") 
+
+print (f"5'{arreglo} 3'")  
+print(f"   {''.join(['|' for nuc in range(len(arreglo))])}") 
+print(f"3' {reverso(arreglo)} 5'")
 
