6b287f8f610224048e33b168ca8d93923966913c
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-05-12 00:18:31 -0500

    Se agrega el paso de argumentos

--- src/porcentajes.py
+++ src/porcentajes.py
@@ -2,7 +2,7 @@
     porcentajes
 
 VERSION
-    1.1
+    1.2
 
 AUTHOR
     César Esparza
@@ -20,20 +20,58 @@ ARGUMENTS
     none
 
 '''
+import argparse
 
-## Solicitar la ruta del archivo 
-ruta = input("Ingrese la ruta del archivo\n")
+arg_parse = argparse.ArgumentParser( description="Calcula los porcentajes de AT y GC")
+
+arg_parse.add_argument("-i", "--input",
+                    metavar="path/to/file",
+                    help="File with gene sequences",
+                    required=True)
+
+arg_parse.add_argument("-o", "--output",
+                    help="Path for the output file",
+                    required=False)
+
+arg_parse.add_argument("-r", "--round",
+                    help="number of digits to rouond",
+                    type=int,
+                    required=False)
+args = parser.parse_args()
+
+ruta = args.input
 
 try:
 
   ## Se abre el archivo para leer
   with open(ruta, 'r') as dna:
-        arreglo= dna.read()
+        arreglo= ruta.read().rstrip("\n").upper()
+	size = len(file_content)
         
 except IOError as io_error: 
     print(f"No se encuentra el archivo {io_error} \n")
   
 else:
+    ## Se saca el porcentaje de la secuencia y se guarda en variables
+    AT = ((arreglo.count('A') + arreglo.count('T')) / size) * 100
+    GC = ((arreglo.count('G') + arreglo.count('C')) / size) * 100
+	 
+    if args.round:
+        AT = round(AT, args.round)
+        GC = round(GC, args.round)
+    
+    if args.output:	
+	file = open(args.output, "w")
+	try:
+   		file.write(f"La secuencia {arreglo}\n Tiene un porcentaje de {AT}% de AT y {GC}% de GC" )
+		print(f"\nSe genero el archivo {args.output} con los resultados")
+	finally:
+		file.close()  
+    
+    else:		
+	print(f"Los porcentajes son:\n  AT:{AT}% y GC:{GC}%" 
+                    
 
-  ##Calcular e imprimir los porcentajes 
-  print(f'Porcentaje de GC y AT\n  AT:{((arreglo.count("A") + arreglo.count("T"))/len(arreglo))*100 }% GC:{((arreglo.count("G") + arreglo.count("C"))/len(arreglo))*100 }%')
+    
+	
+    
