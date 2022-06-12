84b938d398872d8bb2b677257ef8c53caa7205d7
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-06-12 10:19:53 -0500

    comentar el código

--- src/DNA_module.py
+++ src/DNA_module.py
@@ -24,12 +24,16 @@ CATEGORY
 
 
 '''
-
-nucleotides = ["A", "T","C","G"]
-reverse_nucleotides = {"A":"T", "T":"A","C": "G","G":"C"}
-
-#Pasa a mayúsculas y revisar que sean valores validos       
+from structures import nulceotides, reverse_nucleotides
+  
 def validate(dna_seq):
+    '''
+    Pasa a mayúsculas y revisar que sean valores validos 
+        Parameters:
+            dna_seq (str): secuencia de DNA a procesar
+        Returns:
+            tmpseq (str): secueancia de DNA validada o error 
+    '''
   tmpseq = dna_seq.upper()
   for nuc in tmpseq:
     if nuc not in nucleotides:
@@ -37,8 +41,15 @@ def validate(dna_seq):
       return 
   return tmpseq
   
-#Cuenta cuantas veces se repite cada nucleotido
+
 def frequency(seq): 
+      '''
+    Regresa el conteo de nucleotidos en una secuencia de DNA
+        Parameters:
+            seq (str): secuencia de DNA a procesar
+        Returns:
+            tmpFreqDict (dicc): secueancia de DNA validada o error 
+    '''  
   tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
   for nuc in seq: 
      if (nuc in tmpFreqDict): 
@@ -47,17 +58,38 @@ def frequency(seq):
         tmpFreqDict[nuc] = 1 
   return tmpFreqDict
 
-  #Cambia ADN a ARN
+ 
 def transcription(seq):
+          '''
+    Cambia la cadena de DNA una de RNA 
+        Parameters:
+            seq (str): secuencia de DNA a procesar
+        Returns:
+            seq (str): secuancia de RNA
+    '''  
   return seq.replace("T","U")
 
-  # Saca la cadena complementaria  
 def reverse_complement(seq):
+          '''
+    Regresa la cadena complementaria
+        Parameters:
+            seq (str): secuencia de DNA a procesar
+        Returns:
+            seq (seq): secuencia de DNA complentaria a la ingresada
+    '''  
   return"".join( reverse_nucleotides[nuc] for nuc in seq)
 
-  #Regresa el porcentaje de 
-def purine_pyrimidine(seq, dec=0):
   
+def purine_pyrimidine(seq, dec=0):
+          '''
+    Regresa los procentajes de AT y GC 
+        Parameters:
+            seq (str): secuencia de DNA a procesar
+            dec (int): cuantos decimales se desean, por default cero
+        Returns:
+            at_content (float): porcentaje de purinas en la secuencia
+            gc_content (float): porcentaje de pirimidinas en la secuencia
+    '''  
   a_num = seq.count('A') 
   t_num= seq.count('T')
   c_num= seq.count('C') 
@@ -75,13 +107,28 @@ def purine_pyrimidine(seq, dec=0):
 
   return at_content,cg_content
 
-  #Regresa la lectura dell archivo 
+  
 def read(file_path):
+          '''
+    Se encarga de la lectura del archivo 
+        Parameters:
+            file_path (str): ruta al archivo 
+        Returns:
+             (list): lectura por linea del archivo 
+    '''  
   with open(file_path, 'r') as archivo:
     return [l.strip() for l in archivo.readlines()]
    
     #Regresa la traduccion de la secuencia
 def translate(seq, pos_i =0):
+          '''
+    Regresa la traduccion de la secuencia de DNA
+        Parameters:
+            seq (str): secuencia de DNA a procesar
+            pos_i (int): nuc de inicio para cambia marco de lectura
+        Returns:
+            prot (dicc): secuencia peptídica
+    '''  
   prot = []
   for pos in range(pos_i, len(seq) -2, 3): 
     codon = codons[seq[pos:pos+3]]
