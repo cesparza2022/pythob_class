3fdba3f74cd17b789c30b2523e801a9e566dc534
Carlos Michel Mourra Díaz <60996279+MichMourra@users.noreply.github.com>
2022-09-14 20:30:08 -0500

    Sugerencias Tarea 2
    Excelente código 😄 , me alegra que tengas tus funciones y estructuras guardadas en módulos. Eso demuestra que tienes un amplio conocimiento respecto a Python. Solo que hay algunos detalles como errores ortográficos o errores en la estructura que me impidieron correr tu código al principio. Para lograr correr adecuadamente tu código tuve que hacer modificaciones en los tres archivos que utilizas: RNA_to_protein.py, Bio_tools.py, Bio_structures.py. Te sugiero que le eches un vistazo a estos archivos con su respectivo Pull Request. Además te recomiendo que ordenes un poco los archivos de tu repositorio y que agregues lo visto en clase como tags e issues. Fuera de esto considero que tienes un buen entendimiento de lo visto en clase y has resuelto la tarea de muy buena manera. Sigue así 👍

--- src/RNA_to_protein.py
+++ src/RNA_to_protein.py
@@ -1,58 +1,58 @@
 '''
-NAME: 
+NAME:
     RNA_to_Protein
-    
+
 VERSION:
     1.0
-    
-AUTOR: 
+
+AUTOR:
     César Esparza
-    
-DESCRIPTION: 
-    Script que traduce una secuencia de DNA o RNA en 
+
+DESCRIPTION:
+    Script que traduce una secuencia de DNA o RNA en
     una secuencia protéica
-    
-USAGE: 
+
+USAGE:
     python RNA_to_prot.py [-h] -s secuencia RNA
-    
-ARGUMENTS: 
-    -h, --help           
-    -s, --string         
-    -f --file, --file path/to/file 
+
+ARGUMENTS:
+    -h, --help
+    -s, --string
+    -f --file, --file path/to/file
     -o --out_file
-                        
-SOFTWARE REQUIREMENTS: 
+
+SOFTWARE REQUIREMENTS:
     python 3.10
-    
-INPUT: 
+
+INPUT:
     secuencia de RNA o DNA
     archivo con RNA o DNA
-    
-OUTPUT: 
+
+OUTPUT:
     cadena de aminoácidos
 '''
 # Importar libreirias
 import argparse
 from Bio_tools import validate_rna
-from Bio_tools import translate_rna
+from Bio_tools import translate
 from Bio_tools import read
 from Bio_structures import nucleotides
-from Bio_structures import gencode 
+from Bio_structures import gencode
 
 # Pase de argumentos
 arg_parser = argparse.ArgumentParser(description="Translating ARN to protein")
 
-# Obtiene la secuencia de un archivo  
+# Obtiene la secuencia de un archivo
 arg_parser.add_argument("-f", "--file",
                     metavar="path/to/file",
                     help="Archivo con la secuencia de ARN o ADN",
                     required=False)
-              
-# Recibe la secuencia ingresada con el teclado 
+
+# Recibe la secuencia ingresada con el teclado
 arg_parser.add_argument("-s", "--string",
                     help="Secuencia de ARN o ADN",
                     type=str,
-                    required=False)   
+                    required=False)
 
 # Direccion de archivo con el output
 arg_parser.add_argument("-o", "--out_file",
@@ -67,17 +67,17 @@ arguments = arg_parser.parse_args()
 # Lee, valida y traduce
 if arguments.file:
   raw_sequence = read(arguments.file)
-  val_sequence = validate(raw_sequence)
+  val_sequence = validate_rna(raw_sequence)
   peptide = translate(val_sequence)
 
 # Utiliza la secuencia ingresad con el teclado
-# Valida y traduce  
+# Valida y traduce
 if arguments.string:
   raw_sequence = arguments.string
-  val_sequence = validate(raw_sequence)
+  val_sequence = validate_rna(raw_sequence)
   peptide = translate(val_sequence)
-  
-# Escribe el reultado en un archivo 
+
+# Escribe el reultado en un archivo
 if arguments.out_file:
   out_file = open(arguments.out_file, 'w')
   out_file.write(f"La secuencia resultante es:\n{''.join(peptide)}")
@@ -86,12 +86,3 @@ if arguments.out_file:
 # Imprimir la secuencia
 if peptide:
   print(f"La secuencia resultante es:\n {peptide}")
-  
-  
-  
-  
-
-
-
-
-
