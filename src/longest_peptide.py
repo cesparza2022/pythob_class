9f17ea4584ea6d12bc2d3e7e562aa3f6c481f1e3
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-10-04 09:21:06 -0500

    Agregar script longest_peptide

--- src/longest_peptide.py
+++ src/longest_peptide.py
@@ -22,31 +22,33 @@ See also
     None
 '''
 
-from DNA_module import tranaslate
+from DNA_module import tranaslate, validate, seq_read
 from Bio import SeqUtils
 import argparse
 
 
 arg_parser = argparse.ArgumentParser(description="Buscar el peptido mas largo")
                     
-arg_parser.add_argument("-f", "--FILE",
+arg_parser.add_argument("-f", "--file",
                     metavar="path/to/file",
-                    help="Archivo con secuencia de ADN",
+                    help="dirección al archivo",
                     required=False)
 
-arg_parser.add_argument("-s", "--SEQUENCE",
-                    help="secuencia de ADN a procesar",
-                    type = Seq,
+arg_parser.add_argument("-s", "--sequence",
+                    help="secuencia de DNA",
                     required=False)
                     
 arguments = arg_parser.parse_args() 
 
-##Validar la secuencia 
+if arguments.sequence:
+   secuencia = arguments.sequence
+    
+if arguments.file:
+   secuencia = seq_read(arguments.sequence)
 
-##Llamar a la funcion longest_peptide
-
-##Imprimir el resultado 
-
+if validate(secuencia):
+    long_peptide=longest_peptide(seceuncia) 
+    print(f"el peptido de mayor longitud encontrado en la secuencia es {long_peptide}")
 
 
 
